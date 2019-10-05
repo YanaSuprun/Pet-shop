45adabbd3baf62fded382a154c01018e2cc839d6
YanaSuprun <yana.ysuprun@gmail.com>
2019-10-05 18:44:18 +0300

    fixings

--- components/search/ControllerSearch.js
+++ components/search/ControllerSearch.js
@@ -9,47 +9,21 @@ export class ControllerSearch {
     this.init();
   };
 
-  // init() {
-  //   this.view.renderSearch();
-  //   this.view.getInput(this.showListOnInput.bind(this));
-  //   this.eventManager.subscribe('categorySelected', this.showListWithCategories.bind(this));
-  // };
-
-  // showListOnInput(searchParam) {
-  //   this.searchParam = searchParam;
-  //   let data = this.model.getSearchedData(this.searchParam, this.productsCategory);
-  //   this.eventManager.publish('search', data);
-  // };
-
-  // showListWithCategories(productsCategory) {
-  //   this.productsCategory = productsCategory;
-  //   let data = this.model.getSearchedData(this.searchParam, this.productsCategory);
-  //   this.eventManager.publish('searchInCategories', data)
-  // };
-
   init() {
     this.view.renderSearch();
-    this.eventManager.subscribe('categorySelected', this.changeProducts.bind(this));
-    this.view.getInput(this.changeProducts.bind(this));
+    this.view.getInput(this.showListOnInput.bind(this));
+    this.eventManager.subscribe('categorySelected', this.showListWithCategories.bind(this));
   };
 
-  changeProducts(searchParam = 0, productsCategory) {
-    // console.log(this);
+  showListOnInput(searchParam) {
     this.searchParam = searchParam;
-    this.productsCategory = productsCategory;
     let data = this.model.getSearchedData(this.searchParam, this.productsCategory);
     this.eventManager.publish('search', data);
-  }
+  };
 
-  // showListOnInput(searchParam) {
-  //   this.searchParam = searchParam;
-  //   let data = this.model.getSearchedData(this.searchParam, this.productsCategory);
-  //   this.eventManager.publish('search', data);
-  // };
-
-  // showListWithCategories(productsCategory) {
-  //   this.productsCategory = productsCategory;
-  //   let data = this.model.getSearchedData(this.searchParam, this.productsCategory);
-  //   this.eventManager.publish('searchInCategories', data)
-  // };
+  showListWithCategories(productsCategory) {
+    this.productsCategory = productsCategory;
+    let data = this.model.getSearchedData(this.searchParam, this.productsCategory);
+    this.eventManager.publish('searchInCategories', data)
+  };
 }
